0395e78c120e1a061c76cb0ed6c1b3c9471584af
Sanjit Kumar <kumarsanjit2468@gmail.com>
2020-06-06 16:29:02 +0530

    Modifying registration controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'api/student';
-$route['api/registration'] = 'api/registration/index_get';
+$route['api/registration'] = 'api/registration/index_post';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
