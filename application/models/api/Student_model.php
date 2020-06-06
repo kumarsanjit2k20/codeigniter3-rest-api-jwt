e2ecbb59be849c44ac016f7c3384476bb1275d54
Sanjit Kumar <kumarsanjit2468@gmail.com>
2020-06-06 16:40:10 +0530

    Modifying config file

--- application/models/api/Student_model.php
+++ application/models/api/Student_model.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Student_model extends CI_Model {
 
-    function __construct(){
+    public function __construct(){
         parent::__construct();
        
     }
